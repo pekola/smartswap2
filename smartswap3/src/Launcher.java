f78002ad1f00e73fa6caa99dcb26043a2843eaff
asdf <asdf@web.com>
2018-03-14 12:40:09 +0100

    initialaisitions

---
+++ smartswap3/src/Launcher.java
@@ -0,0 +1,11 @@
+import java.util.UUID;
+import java.util.UUID.*;
+
+public class Launcher {
+	
+	public static void main(String[] args) {
+		String address = UUID.randomUUID().toString();
+		System.out.println(address);
+	}
+
+}
